9bf957b1484c56a1a93fe90b5c528e0586e07ced
cayto mahamed <aidarous1143@gmail.com>
2022-12-14 09:09:29 -0800

    😌 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-	char abe[] = "_putchar";
+        char abe[] = "_putchar";
 
-	int c;
+        int c;
 
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
-	_putchar('\n');
-	return (0);
+        for (c = 0; c < 8; c++)
+        {
+                _putchar(abe[c]);
+        }
+        _putchar('\n');
+        return (0);
 }
