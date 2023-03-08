4a8ce8c955fbb8b0705291dfa68a1e13b7c3d40f
cayto <aidarous1143@gmail.com>
2023-03-08 19:48:02 +0300

    👩‍💻 print last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,17 +1,19 @@
 #include <limits.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(INT_MIN);
-	_putchar('0' + r);
+	
+	print_last_digit(INT_MIN);
+	
+	print_last_digit(8);
+	print_last_digit(-2147483648);
 	_putchar('\n');
 	return (0);
 }
