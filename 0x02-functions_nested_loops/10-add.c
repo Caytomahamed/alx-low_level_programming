63cb0cae1534045996cc55e91d8accfa2c4d8123
cayto <aidarous1143@gmail.com>
2023-03-09 10:25:50 +0300

    🐪 add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * add - print sum of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Retrun: sum of the two numbers
+*/
+
+int add(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
