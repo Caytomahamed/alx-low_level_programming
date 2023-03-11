271b6581487521e3bc12f7b39e7fd5271ea3d462
cayto <aidarous1143@gmail.com>
2023-03-11 18:23:55 +0300

    ⚛  print first 50 terms of fibonacci start with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * fibonacci - print sequence of numbers
+ *
+ * Reterun: fibonacci number
+ */
+
+int main()
+{
+	long int i = 1;
+	long int n1 = 1;
+	long int n2 = 2;
+	long int nextTerm;
+
+	while (i <= 50)
+	{
+		if(i != 50)
+			printf("%lu, ",n1);
+		else 
+			printf("%lu", n1);
+		nextTerm = n1 + n2;
+		n1 = n2;
+		n2 = nextTerm;
+		i++;
+	}
+       
+	return (0);
+}
