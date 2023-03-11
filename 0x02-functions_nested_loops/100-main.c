d1f995da3ba89bf849ca8b458490a85583a28e3e
cayto <aidarous1143@gmail.com>
2023-03-11 17:47:28 +0300

    😂 refactor

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -7,12 +7,9 @@
  */
 int main(void)
 {
-    print_times_table(3);
+    print_times_table(13);
     _putchar('\n');
-    print_times_table(5);
+    print_times_table(12);
     _putchar('\n');
-    print_times_table(0);
-    _putchar('\n');
-    print_times_table(12);  
     return (0);
 }
