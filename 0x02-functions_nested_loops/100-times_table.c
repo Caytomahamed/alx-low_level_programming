00a1f585d599692e173670cce9c447e299bda87e
cayto <aidarous1143@gmail.com>
2023-03-11 15:29:48 +0300

    💈 9 table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,20 +9,23 @@
 
 void print_times_table(int n)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int times = i * j;
-            if (j == n)
-                printf("%3d", times);
-            else
-                printf("%3d, ", times);
+	if(n < 15 && n < 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int times = i * j;
+				if (j == n)
+					printf("%3d", times);
+				else
+					printf("%3d, ", times);
 
-        }
-        printf("\n");
-    }
+			}
+			printf("\n");
+		}
+	}
 }
