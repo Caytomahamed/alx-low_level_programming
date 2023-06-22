7e83f755553141c6b057cc88b946744be0b44cb4
Aidarous <aidarous1143@gmail.com>
2023-06-22 02:10:54 -0700

    🍕 update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int positive_or_negative(int n)
+void positive_or_negative(int n)
 {
 	if (n == 0)
 		printf("%d is zero\n", n);
@@ -15,7 +13,5 @@ int positive_or_negative(int n)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is positive\n", n);
-
-	return (0);
 
 }
