ed1d1492d403c4ef03ae9b8004e331ad5a08a141
Aidarous <aidarous1143@gmail.com>
2023-06-26 15:17:40 +0300

    🍗 get length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - Get length of string
+ * @s: is string that count is length
+ *
+ * Return: length of string
+*/
+int _strlen(char *s)
+{
+    int i;
+    int len;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        len = i;
+    }
+
+    return (len + 1);
+}
