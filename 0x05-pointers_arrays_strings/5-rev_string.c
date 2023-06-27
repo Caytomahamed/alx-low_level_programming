a087c27ce951037f17aa4da055ee711354436824
Aidarous <aidarous1143@gmail.com>
2023-06-27 04:11:56 +0300

    🍕 rev string code format

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * rev_string - reverse string
  * @s: string
@@ -19,6 +20,7 @@ void rev_string(char *s)
 	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
 		char temp = s[i];
+
 		s[i] = s[j];
 		s[j] = temp;
 	}
