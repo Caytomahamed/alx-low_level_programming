808e759d5a20fe7431bf6eef071a1a6268bf6685
Aidarous <aidarous1143@gmail.com>
2023-06-29 20:33:26 +0300

    🍟 betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int index = strlen(dest);
-    int a = 0;
+	int index = strlen(dest);
+	int a = 0;
 
-    while (a < n && *src)
-    {
-        dest[index + a] = *src;
-        src++;
-        a++;
-    }
-    dest[index + a] = '\0';
-    return (dest);
+	while (a < n && *src)
+	{
+		dest[index + a] = *src;
+		src++;
+		a++;
+	}
+	dest[index + a] = '\0';
+	return (dest);
 }
