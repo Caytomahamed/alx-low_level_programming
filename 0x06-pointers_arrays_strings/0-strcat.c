f017cb9d604bc299ab4694b4c0837730334c4498
Aidarous <aidarous1143@gmail.com>
2023-06-29 20:05:57 +0300

    🍔 strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+char *_strcat(char *dest, char *src)
+{
+    char *p = dest;
+
+    /*Move the pointer to the end of dest */
+    while (*p != '\0')
+    {
+        p++;
+    }
+
+    /* Append src to dest*/
+    while (*src != '\0')
+    {
+        *p++ = *src++;
+    }
+
+    /*Add a terminating null byte*/
+    *p = '\0';
+    return (dest);
+}
