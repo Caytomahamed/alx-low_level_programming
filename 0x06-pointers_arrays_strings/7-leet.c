f92ed4e4bb538a958e7c2e788b6cd2d1879c5cb5
Aidarous <aidarous1143@gmail.com>
2023-06-29 20:41:36 +0300

    🥤 leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,28 +6,28 @@
  */
 char *leet(char *s)
 {
-    int i;
-    int j;
-    int k;
+	int i;
+	int j;
+	int k;
 
-    char l[] = "oOlLeEaAtT";
-    char e[] = "0011334477";
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
 
-    i = 0;
-    while (s[i] != '\0')
-    {
-        j = 0;
-        k = 0;
-        while (l[j] != '\0')
-        {
-            if (s[i] == l[j])
-            {
-                k = j;
-                s[i] = e[k];
-            }
-            j++;
-        }
-        i++;
-    }
-    return (s);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
+		{
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
 }
