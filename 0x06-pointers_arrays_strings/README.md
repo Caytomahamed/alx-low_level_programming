a6a5d5ad6b34e0efc6e409c7f756eb21c57360f9
Aidarous <aidarous1143@gmail.com>
2023-06-29 17:49:54 +0300

    🍔 done

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#  More pointers, arrays and strings
+
+ More pointers, arrays and strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void print_buffer(char *b, int size);
+
+#endif
