e9b5b8afead19f17cf95fff9c348d07259058082
Aidarous <aidarous1143@gmail.com>
2023-06-30 05:38:42 +0300

    🔢 print numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -10,6 +11,7 @@ char *rot13(char *s);
 char *string_toupper(char *n);
 char *leet(char *s);
 char *rot13(char *s);
+void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
