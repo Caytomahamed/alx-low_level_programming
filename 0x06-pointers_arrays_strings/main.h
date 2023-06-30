6c3d1e62a463d9b7f9d16a86615821a6b7e1f266
Aidarous <aidarous1143@gmail.com>
2023-06-30 05:27:55 +0300

    🍟 betty checking

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,9 +2,13 @@
 #define HEADER_FILE
 
 char *_strcat(char *dest, char *src);
-
-
-
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *n);
+char *cap_string(char *s) char *rot13(char *s);
+char *string_toupper(char *n);
+char *leet(char *s);
+char *rot13(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
