9628e6d0f793e1d4926e934911a6a4d4c36c90b7
Aidarous <aidarous1143@gmail.com>
2023-07-03 18:46:49 +0300

    📈strchr or substring

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * _strchr -  is used to find the first occurrence of a character
+ * in a null-terminated string.
+ * @s: A pointer to a null-terminated string to search.
+ * @c: An integer value representing the character to search for.
+ *
+ * Return: A pointer if c is found otherwise NULL
+ */
 char *_strchr(char *s, char c)
 {
 	int len;
@@ -11,13 +18,13 @@ char *_strchr(char *s, char c)
 	index = -1;
 
 	/*Calculte the length of string*/
-	while(s[len] != '\0')
+	while (s[len] != '\0')
 		len++;
 
 	/*find index of character of c*/
-	while(n < len)
+	while (n < len)
 	{
-	    if(s[n] == c)
+		if (s[n] == c)
 		{
 			index = n;
 			break;
@@ -26,8 +33,8 @@ char *_strchr(char *s, char c)
 	}
 
 	/*if character is not found return NULL*/
-	if(index == -1)
-		return NULL;
+	if (index == -1)
+		return (NULL);
 
-	return s + index;
+	return (s + index);
 }
