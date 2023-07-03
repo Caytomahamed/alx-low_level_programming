33e7b8a3e44acf5d51789dcd144fe27466ac823e
Aidarous <aidarous1143@gmail.com>
2023-07-03 11:56:53 +0300

    🔨memcpy fuction

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/**
+* _memcpy - used to copy a block of memory from one location
+* to another.
+* @dest: is pointer to the memory location where the block
+* memory should be copied.
+* @src: is pointer to the memory location from which the
+* block of memory should be memory.
+* @n: is the number of bytes to be copied.
+*
+* Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(dest + i) = src[i];
+	}
+
+	return (dest);
+}
