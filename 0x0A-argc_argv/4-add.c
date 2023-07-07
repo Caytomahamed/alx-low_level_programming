635a8758cccfc2bb40b7da92b1500535b45503a0
Aidarous <aidarous1143@gmail.com>
2023-07-07 20:38:39 +0300

    ➕ add infinity numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - add infinity numbers.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num;
+	int result;
+	int i;
+
+	result = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", result);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+
+		if (num == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
