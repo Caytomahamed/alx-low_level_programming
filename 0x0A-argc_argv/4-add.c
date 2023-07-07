814a20d122431ca780fcef6a71a171958a4231b2
Aidarous <aidarous1143@gmail.com>
2023-07-07 20:55:52 +0300

    🚗 update check is digit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,29 +12,28 @@
 
 int main(int argc, char *argv[])
 {
-	int num;
-	int result;
+	int result = 0;
+	long num;
+	char *endptr;
 	int i;
-
-	result = 0;
 
 	if (argc == 1)
 	{
 		printf("%d\n", result);
-		return (1);
+		return 0;
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-
-		if (num == 0)
+		num = strtol(argv[i], &endptr, 10);
+		if (*endptr != '\0' || endptr == argv[i])
 		{
 			printf("Error\n");
-			return (1);
+			return 1;
 		}
 		result += num;
 	}
+
 	printf("%d\n", result);
-	return (0);
+	return 0;
 }
