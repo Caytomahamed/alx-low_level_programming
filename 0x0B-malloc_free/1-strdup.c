2586694c38a7e01fad7a7fbfb07395b3a2a66149
Aidarous <aidarous1143@gmail.com>
2023-07-11 20:21:07 +0300

    👮‍♀️ copy string into new address

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: a pointer to the new copy of the string, or NULL if allocation fails
+ */
+char *_strdup(char *str)
+{
+	unsigned int len = 0;
+	char *new_copy, *copy_start;
+
+	while (str[len] != '\0')
+		++len;
+
+	if (len == 0)
+		return (NULL);
+
+	new_copy = malloc(sizeof(char) * (len - 1));
+
+	if (new_copy)
+	{
+		copy_start = new_copy;
+
+		while (*str != '\0')
+			*new_copy++ = *str++;
+
+		*new_copy = '\0';
+		return (copy_start);
+	}
+
+	return (NULL);
+}
