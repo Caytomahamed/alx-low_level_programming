5342cf7a0b07b64a2a81abc655cce3da051df6c4
Aidarous <aidarous1143@gmail.com>
2023-07-13 14:36:40 +0300

    🚠 concatenates two string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,31 +2,32 @@
 #include <string.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size of the memory block to be allocated
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 to concatenate
  *
- * Return: pointer to the allocated memory
- *         exit with status value of 98 if malloc fails
+ * Return: pointer to the concatenated string on success, or NULL on failure
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int len1 = 0, len2 = 0;
+	char *result;
+	unsigned int len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    len1 = strlen(s1);
-    len2 = strlen(s2);
-    if (n >= len2)
-        n = len2;
-    result = malloc(len1 + n + 1);
-    if (result == NULL)
-        return (NULL);
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, n);
-    result[len1 + n] = '\0';
-    return (result);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2)
+		n = len2;
+	result = malloc(len1 + n + 1);
+	if (result == NULL)
+		return (NULL);
+	memcpy(result, s1, len1);
+	memcpy(result + len1, s2, n);
+	result[len1 + n] = '\0';
+	return (result);
 }
