687aa92b9049998fe9c931009e63a8923eeb11e3
Aidarous <aidarous1143@gmail.com>
2023-07-17 17:37:54 +0300

    🚨 add betty doc

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,9 +5,10 @@
  *
  * Description: This function prints the name of the file
  * it was compiled from, followed by a new line.
+ * Return: always 0(success)
  */
 int main(void)
 {
-    printf("%s\n",__FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
