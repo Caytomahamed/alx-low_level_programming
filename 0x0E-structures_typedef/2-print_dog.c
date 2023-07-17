0a0069b36f00d67bf8b4aed481c142738c19ccc1
Aidarous <aidarous1143@gmail.com>
2023-07-17 23:07:24 +0300

    🚨 betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
-
+/**
+ * print_dog - print information about a dog
+ * @d: pointer to the dog structure to print
+ *
+ * Description: print the name, age, and owner of the given dog structure
+ */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 
-    printf("Name: %s\n", d->name);
-    printf("Age: %f\n", d->age);
-    printf("Age: %f\n", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->owner);
 }
