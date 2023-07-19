0367d9d206c6dd42de9b527da490212a7d63fb9f
Aidarous <aidarous1143@gmail.com>
2023-07-19 22:59:20 +0300

    🎉 change condition that checking if two string is equal

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <string.h>
+
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: the operator passed as argument to the program
@@ -21,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*s == *(ops[i].op))
+		if (strcmp(s, *(ops[i].op)) == 0)
 			return (ops[i].f);
 		i++;
 	}
