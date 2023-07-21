2022ede88d369cb9aaa0626b6d8b2778914f4c22
Aidarous <aidarous1143@gmail.com>
2023-07-21 05:48:55 +0300

    📖 refactor

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,11 +13,55 @@ void print_all(const char *const format, ...)
 	char *p;
 
 	va_start(list, format);
-	for (j = 0; format && format[j] != '\0'; j++)
+	while (format && format[j] != '\0')
 	{
-		print_specifier(format[j], list, &start);
+		switch (format[j])
+		{
+		case 'c':
+			switch (start)
+			{
+			case 1:
+				printf(", ");
+			}
+			start = 1;
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			switch (start)
+			{
+			case 1:
+				printf(", ");
+			}
+			start = 1;
+			printf("%i", va_arg(list, int));
+			break;
+		case 'f':
+			switch (start)
+			{
+			case 1:
+				printf(", ");
+			}
+			start = 1;
+			printf("%f", va_arg(list, double));
+			break;
+		case 's':
+			switch (start)
+			{
+			case 1:
+				printf(", ");
+			}
+			start = 1;
+			p = va_arg(list, char *);
+			if (p)
+			{
+				printf("%s", p);
+				break;
+			}
+			printf("%p", p);
+			break;
+		}
+		j++;
 	}
-
 	printf("\n");
 	va_end(list);
 }
