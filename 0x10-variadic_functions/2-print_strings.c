05eaa40ada41683ed26d900a8dd8c88061a36c08
Aidarous <aidarous1143@gmail.com>
2023-07-21 05:29:59 +0300

    🖨 print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,15 +1,14 @@
 #include "variadic_functions.h"
 
-	/**
-	 * print_strings - Print numbers using a separator
-	 * @separator: The string separator
-	 * @n: The quantity of numbers
-	 * @...: The list of numbers
-	 *
-	 * Return: Nothing
-	 */
-	void
-	print_strings(const char *separator, const unsigned int n, ...)
+/**
+ * print_strings - Print numbers using a separator
+ * @separator: The string separator
+ * @n: The quantity of numbers
+ * @...: The list of numbers
+ *
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
